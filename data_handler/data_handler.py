b628c23ef3d7082c4be5119b7b39c53fd777d16d
sotpotatis <35110380+William04A@users.noreply.github.com>
2022-04-06 20:29:54 +0200

    ...and fix another bug!

--- data_handler/data_handler.py
+++ data_handler/data_handler.py
@@ -18,7 +18,7 @@ def upload_homework_file(new_content:list):
 
     :param new_content: The new content to upload to the server.'''
     #We want the list to be in order so that the newest homework appears first, and vice versa.
-    date_to_datetime = lambda assignment: datetime.datetime.strptime(assignment["due"], "%Y-m-d")
+    date_to_datetime = lambda assignment: datetime.datetime.strptime(assignment["due"], "%Y-%m-%d")
     logger.info("Sorting homework file content by date...")
     new_content = new_content.sort(key=date_to_datetime)
     logger.info("Homework file sorted by date.")
